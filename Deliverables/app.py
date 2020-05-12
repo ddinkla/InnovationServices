ef264ca8896dfef511c6abe9b790ae3db282bd69
ddinkla <33684942+ddinkla@users.noreply.github.com>
2020-05-12 16:54:37 +0200

    Initial Commit Trip_Request Service

--- Deliverables/app.py
+++ Deliverables/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(TripRequests, '/trip_requests/', methods=['POST'])
-api.add_resource(TripRequest, '/trip_requests/<string:name>', methods=['GET', 'PUT', 'DELETE'])
+api.add_resource(TripRequest, '/trip_requests/<string:trip_id>', methods=['GET', 'PUT', 'DELETE'])
 
 app.run(host='0.0.0.0', port=5000, debug=True)
 # In the context of servers, 0.0.0.0 can mean "all IPv4 addresses on the local machine".
