51b971e98e9b988f00975c8d80428562ba4b9d0d
ddinkla <33684942+ddinkla@users.noreply.github.com>
2020-05-13 19:28:54 +0200

    Split out driver_location and driver_details service, updated macro get

---
+++ Deliverables/resources/driver_details.py
@@ -0,0 +1,86 @@
+from flask_restful import Resource, reqparse
+from flask import request
+
+# dummy data
+drivers = [
+    {
+        "driver_id": "d12542",
+        "name": "Fernando Alonso",
+        "status": "offline",
+        "CarType": "basic"
+    },
+    {
+        "driver_id": "d85719",
+        "name": "Daniel Ricciardo",
+        "status": "available",
+        "CarType": "basic"
+    },
+    {
+        "driver_id": "d54818",
+        "name": "Lewis Hamilton",
+        "status": "driving",
+        "CarType": "premium"
+    },
+    {
+        "driver_id": "d762057",
+        "name": "Max Verstappen",
+        "status": "driving",
+        "CarType": "premium"
+    }
+]
+
+
+# resource place record
+class DriverRecord(Resource):
+    def get(self, driver_id):
+        for driver in drivers:
+            if driver_id == driver["driver_id"]:
+                return driver, 200  # return 200 HTTP status code to indicate success
+        return {"message": "Driver not found"}, 404  # return 404 HTTP status code to indicate resource not found
+
+    def put(self, driver_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("status", type=str, help="Status not valid, change driver status",
+                            choices=['available', 'driving', 'offline'])
+        args = parser.parse_args(strict=True)
+
+        # possible_status = ['available', 'driving', 'offline']
+        # if args["Status"] not in possible_status:
+        #    return {"message": "Bad request, status not in status format (available, driving, offline)"}, 400
+        # else:
+
+        for driver in drivers:
+            if driver_id == driver["driver_id"]:
+                driver["status"] = args["status"]
+                return driver, 200
+
+        return {"message": "Trip not found"}, 404
+
+    def delete(self, driver_id):
+        to_be_deleted = None
+        for driver in drivers:
+            if driver_id == driver["driver_id"]:
+                to_be_deleted = driver
+                break
+
+        if to_be_deleted:
+            drivers.remove(to_be_deleted)
+            return "{} is deleted.".format(driver_id), 200
+        return {"message": "Driver not found"}, 404
+
+
+# resource collection Drivers
+class DriverRecords(Resource):
+    def post(self):
+        driver_to_be_created = request.get_json(force=True)
+        driver_id = driver_to_be_created["driver_id"]
+        for driver in drivers:
+            if driver_id == driver["driver_id"]:
+                # 500 Internal Server Error HTTP status code
+                return {"message": "Driver with ID {} already exists".format(driver_id)}, 500
+
+        drivers.append(driver_to_be_created)
+        return driver_to_be_created, 201  # 201 Created HTTP status code
+
+    def get(self):
+            return drivers, 200
--- Deliverables/resources/driver_location.py
+++ Deliverables/resources/driver_location.py
@@ -2,91 +2,77 @@ from flask_restful import Resource, reqparse
 from flask import request
 
 # dummy data
-drivers = [
+driver_locations = [
     {
         "driver_id": "d12542",
-        "name": "Fernando Alonso",
         "last_location": [51.680400, 5.293250],
-        "status": "offline",
-        "CarType": "basic"
     },
     {
         "driver_id": "d85719",
-        "name": "Daniel Ricciardo",
         "last_location": [51.676213, 5.289145],
-        "status": "available",
-        "CarType": "basic"
     },
     {
         "driver_id": "d54818",
-        "name": "Lewis Hamilton",
         "last_location": [51.64751, 5.310581],
-        "status": "driving",
-        "CarType": "premium"
     },
     {
         "driver_id": "d762057",
-        "name": "Max Verstappen",
         "last_location": [51.71853, 5.2960],
-        "status": "driving",
-        "CarType": "premium"
     }
 ]
 
 
 # resource place record
-class DriverRecord(Resource):
+class DriverLocation(Resource):
     def get(self, driver_id):
-        for driver in drivers:
+        for driver in driver_locations:
             if driver_id == driver["driver_id"]:
                 return driver, 200  # return 200 HTTP status code to indicate success
         return {"message": "Driver not found"}, 404  # return 404 HTTP status code to indicate resource not found
 
     def put(self, driver_id):
         parser = reqparse.RequestParser()
-        parser.add_argument("status", type=str, help="Status not valid, change driver status",
-                            choices=['available', 'driving', 'offline'])
+        parser.add_argument("last_location_lat", type=float, help="Change last location latitude")
+        parser.add_argument("last_location_long", type=float, help="Change last location longitude")
         args = parser.parse_args(strict=True)
 
-        # possible_status = ['available', 'driving', 'offline']
-        # if args["Status"] not in possible_status:
-        #    return {"message": "Bad request, status not in status format (available, driving, offline)"}, 400
-        # else:
+        if args["last_location_lat"] is None or args["last_location_long"] is None:
+            return {"message": "Invalid location, latitude or longitude is None"}, 400
 
-        for driver in drivers:
+        for driver in driver_locations:
             if driver_id == driver["driver_id"]:
-                driver["status"] = args["status"]
+                driver["last_location"][0] = args["last_location_lat"]
+                driver["last_location"][1] = args["last_location_long"]
                 return driver, 200
 
-        return {"message": "Trip not found"}, 404
+        return {"message": "Place record not found"}, 404
 
     def delete(self, driver_id):
         to_be_deleted = None
-        for driver in drivers:
+        for driver in driver_locations:
             if driver_id == driver["driver_id"]:
                 to_be_deleted = driver
                 break
 
         if to_be_deleted:
-            drivers.remove(to_be_deleted)
+            driver_locations.remove(to_be_deleted)
             return "{} is deleted.".format(driver_id), 200
         return {"message": "Driver not found"}, 404
 
 
 # resource collection Drivers
-class DriverRecords(Resource):
+class DriverLocations(Resource):
     def post(self):
-        driver_to_be_created = request.get_json(force=True)
-        driver_id = driver_to_be_created["driver_id"]
-        for driver in drivers:
+        location_to_be_created = request.get_json(force=True)
+        driver_id = location_to_be_created["driver_id"]
+        for driver in driver_locations:
             if driver_id == driver["driver_id"]:
                 # 500 Internal Server Error HTTP status code
                 return {"message": "Driver with ID {} already exists".format(driver_id)}, 500
 
-        drivers.append(driver_to_be_created)
-        return driver_to_be_created, 201  # 201 Created HTTP status code
+        driver_locations.append(location_to_be_created)
+        return location_to_be_created, 201  # 201 Created HTTP status code
 
-
-class DriversRecords(Resource):
     def get(self):
-            return drivers, 200
+            return driver_locations, 200
+
